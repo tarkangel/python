ea1a8fa53ab4efd83fdf9e4d9dc61c4620ed7a58
tarkangel <angel_calderon5@hotmail.com>
2022-07-12 23:32:37 -0500

    Added conversor.py

---
+++ conversor.py
@@ -0,0 +1,12 @@
+
+
+import re
+
+
+mxn_peso = int(input("Introduce Mexican Pesos value : "))
+usd_dollar = 20
+
+result = (mxn_peso / usd_dollar)
+
+
+print (str(mxn_peso) + " pesos worth " + str(result) + " US Dollars")
