5217fe67c6c0e255c3dcf1a32db2495d16830985
tarkangel <angel_calderon5@hotmail.com>
2022-07-15 10:49:32 -0500

    Added functions.py and hola_mundo.py

---
+++ functions.py
@@ -0,0 +1,17 @@
+
+
+from cmath import exp
+
+
+def exponential_function (number):
+
+    result = number * number 
+    print("The result is :" + (str(result)))
+    return result
+
+input = int(input("Introduce a number to exponential : "))
+exponential_function(input)
+
+
+
+    
